ded3f3580d4a36ced052351b79c9e58caa2e79f1
Ali Gamal Ali <hictors@gmail.com>
2020-08-05 22:05:10 +0200

    adding functionality to the darkmode toggler to change the name

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,5 +1,13 @@
 const inputCheck = document.getElementById("switch");
+const switchLabel = document.getElementById("darkLabel");
 
-inputCheck.addEventListener("change", () => {
+const toggleDarkMode = () => {
   document.body.classList.toggle("darkTheme");
-});
+  if (document.body.className === "darkTheme") {
+    switchLabel.textContent = "Light Mode";
+  } else {
+    switchLabel.textContent = "Dark Mode";
+  }
+};
+
+inputCheck.addEventListener("change", toggleDarkMode);
